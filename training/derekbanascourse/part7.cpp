ea6d29ae16f14548d449575e07f0a233898d111f
Jorge Banguera <jorge.banguera@YUXIDEV-JBANGUERA-2.local>
2017-01-04 10:35:38 -0500

    Strings:final

--- training/derekbanascourse/part7.cpp
+++ training/derekbanascourse/part7.cpp
@@ -27,5 +27,24 @@ int main(){
     << "String is empty: emptyStr.empty() = " << emptyStr.empty() << "\n\n"
     << "Double to String coversion: stod(\"0.1234\") + 1 = " << stod("0.1234") + 1 << "\n\n"
     << endl;
+
+
+  string sCat = "cat";
+  string sDog = "dog";
+  string fullName = "jorge felix";
+  string halfName;
+
+  cout << "Example: using extra methods: compare, assign, find, insert, erase, replace ... check the code ;)" << endl;
+  cout
+    << "Comparing strings \"cat\".compare(\"cat\") =  " << sCat.compare(sCat) << "\n\n"
+    << "Comparing strings \"cat\".compare(\"dog\") =  " << sCat.compare(sDog) << "\n\n"
+    << "Substring with assign method, halfName.assign(fullName,0,5) = " << halfName.assign(fullName,0,5) << "\n\n"
+    << "fullName = " << fullName << " - halfName = " << halfName << "\n\n"
+    << "finding index with find: fullName.find(\"felix\", 0) = " << fullName.find("felix") << "\n\n"
+    << "insert string: fullName.inser(11, \" banguera\") = " << fullName.insert(11, " banguera") << "\n\n"
+    << "erase string: fullName.erase(5, 6) = " << fullName.erase(5, 6) << "\n\n"
+    << "replace string: fullName.replace(0, 1, \"J\").replace(6, 1, \"B\") = " << fullName.replace(0, 1, "J").replace(6, 1, "B")  << "\n\n"
+    << "The final fullName: " << fullName
+    << endl;
   return 0;
 }
